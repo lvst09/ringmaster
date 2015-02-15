8b78e41e675d69c544fd99f4f6f23909f0059e22
fishlegs <sky@skytekiMacBook-Air.local>
2015-02-15 23:20:07 +0800

    1.add scenekit project

---
+++ Ring/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ring
+//
+//  Created by sky on 15/2/8.
+//  Copyright (c) 2015年 NoName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ring/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Ring
+//
+
+//  Copyright (c) 2015年 NoName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
