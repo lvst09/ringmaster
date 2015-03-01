78d8abe8ae1880a5d1d3ca02c88bc3f86e0ad8e1
lvst09 <lvst09@gmail.com>
2015-03-01 15:43:47 +0800

    detect finger

--- iOS/catRing/catRing/FirstViewController.mm
+++ iOS/catRing/catRing/FirstViewController.mm
@@ -42,6 +42,9 @@
 
 @property (nonatomic, assign) NSInteger totalVideoFrame;
 
+
+@property (nonatomic, assign) int imageIndex;
+
 @end
 
 @implementation FirstViewController
@@ -86,6 +89,8 @@
     [super viewDidAppear:animated];
     
     [self getAllImageFromVideo];
+    
+    [self showImageAtIndex:1];
 }
 
 #pragma mark -
@@ -127,6 +132,8 @@
     
     NSLog(@"width=%d, height=%d", ipImage->width, ipImage->height);
     HandGesture hg;
+    
+    hg.index = self.imageIndex;
     MyImage * myImage = detectHand(ipImage, hg);
     NSLog(@"width=%d, height=%d", myImage->src.cols, myImage->src.rows);
     IplImage qImg;
@@ -154,8 +161,12 @@
 //    NSString *fileName = [NSString stringWithFormat:@"MYIMG_SMALL%zd.JPG", j];
     NSLog(@"current filename=%@", betaCompressionDirectory);
 //    image = [UIImage imageNamed:fileName];
+    
+    self.imageIndex = j;
+    
     image = [UIImage imageWithContentsOfFile:betaCompressionDirectory];
     self.imageView.image = [self processImage:image];
+    
 }
 
 
