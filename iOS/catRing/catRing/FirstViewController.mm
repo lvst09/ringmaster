42e0e26b412dbd4882b34c0aefebaa5dcf1ecf9e
fishlegs <sky@skytekiMacBook-Air.local>
2015-04-05 18:02:18 +0800

    显示戒指

--- iOS/catRing/catRing/FirstViewController.mm
+++ iOS/catRing/catRing/FirstViewController.mm
@@ -221,7 +221,7 @@
         [self.indicator startAnimating];
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self getAllImageFromVideo];
-//            [self processAllImages];
+            [self processAllImages];
             [self.indicator stopAnimating];
             [self showImageAtIndex:1];
         });
